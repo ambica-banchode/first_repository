2467bca673c48edade91b9eadead034eadd336fb
ambica <shonu.1409@gmail.com>
2020-04-07 16:27:57 +0530

    third revision

--- index.cpp
+++ index.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main()
 {
-    cout<<"this is a simple hello world program"<<endl;
+    cout<<"this is a simple hello world program. trying to make chnages to this and test"<<endl;
     return 0;
 }
